e0ce668880430d57b02703d30fb1ceaf76f66375
Ethan Huston <ethanghuston@gmail.com>
2020-12-08 16:38:13 -0500

    Use TEXT macro for multi platform

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,11 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to the Bull Cow Game");
-    PrintLine("Press enter to continue...");
+    PrintLine(TEXT("Welcome to the Bull Cow Game!"));
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
+    ClearScreen(); 
+    FString HiddenWord = TEXT("ethan");
 }
