dd8fff88016a46753f46026e809ca6f58933c8c8
Ethan Huston <ethanghuston@gmail.com>
2020-12-08 16:25:38 -0500

    Continue to work on the terminal

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Hi There!");
+    PrintLine("Welcome to the Bull Cow Game");
+    PrintLine("Please enter a string.");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
