dafde40ed36fe591adc60198e18fc1ba73546b17
Ethan Huston <ethanghuston@gmail.com>
2020-12-08 21:09:47 -0500

    Adding "win lose"

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,24 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
+    PrintLine(TEXT("Guess the 4 letter word")); //Replace magic number!!!
     PrintLine(TEXT("Press enter to continue..."));
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen(); 
-    FString HiddenWord = TEXT("ethan");
-    PrintLine(Input);
+    FString HiddenWord = TEXT("cake"); //Move outside this function
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You've guessed correctly! You win!"));
+    } 
+    else
+    {
+        PrintLine(TEXT("Wrong answer!"));
+    }
+    
+    //PrintLine(Input);
 }
